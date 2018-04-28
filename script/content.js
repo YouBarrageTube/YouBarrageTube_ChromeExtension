ba575e955878f913d02d58587b1a17a8076eaede
LiangjunSong <liangjun_song@yeah.net>
2018-04-28 00:14:53 -0600

    new danmu with jQuery

--- script/content.js
+++ script/content.js
@@ -1,48 +1,84 @@
 //console.log('hi');
-var commentsContainer='<div class="comments-container"></div>';
+// var commentsContainer = '<div class="comments-container"></div>';
 
-$('.html5-video-player').append(commentsContainer);
+// $(".html5-video-player").append(commentsContainer);
 
-var container = $('.comments-container');
+// var container = $(".comments-container");
 
 //console.log(container);
 
-var comments = [];
+// console.log(video);
 
-var currentIndex = 0;
+// var video;
 
-var video = $('video')[0];
+var barrage = new Barrage(container, []);
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    comments = request.comments;
-    currentIndex = 0;
-    container.empty();
-});
+var startDanmu = function(comments){
+  var container;
 
-var getDuration = function(){
-  var temp = Math.random() * 10;
-  if (temp < 1.5) return 'slow';
-  if (temp > 8.5) return 'fast';
-  else return 'medium';
-};
+  var video;
 
-var getCommentHTML = function(content,currentIndex){
-  return '<div class = "comment '+ getDuration() +'" style="top:'+ (currentIndex%15+1) * 5.5 +'%">'+content+'</div>'
+  if($(".comments-container").length === 0){
+    var commentsContainer = '<div class="comments-container"></div>';
+
+    $(".html5-video-player").append(commentsContainer);
+  
+    video = $("video")[0];
+
+    video.ontimeupdate = function() {
+      var currentTime = Math.floor(video.currentTime);
+      if (currentTime === 0 || barrage.currentTime !== currentTime) {
+        barrage.currentTime = currentTime;
+        barrage.popDanmu();
+      }
+    };
+  
+    video.onpause = function() {
+      barrage.pause();
+    };
+  
+    video.onplay = function() {
+      barrage.resume();
+    };
+  
+    video.onseeked = function() {
+      var currentTime = Math.floor(video.currentTime);
+      barrage.currentIndex = 0;
+      barrage.currentTime = currentTime;
+    };
+  }
+  container = $(".comments-container");
+  container.empty();
+  console.log(container);
+  barrage.update(container, comments);
 }
 
-var updateCurrentComments = function(currentTime){
-  var num = 0;
-  while (currentIndex < comments.length && comments[currentIndex].videoTime <= currentTime){
-    if(currentTime === comments[currentIndex].videoTime && num<15){
-      container.append(getCommentHTML(comments[currentIndex].comment, currentIndex));
-      num ++;
-    }
-    currentIndex ++;
-  };
-};
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  startDanmu(request.comments);
 
-setInterval(function(){
-  // console.log(comments[0])
-  updateCurrentComments(Math.floor(video.currentTime));
-},500);
+  console.log(barrage.comments);
+});
+
+// video.onPlay
+
+// var getDuration = function(){
+//   var temp = Math.random() * 10;
+//   if (temp < 1.5) return 'slow';
+//   if (temp > 8.5) return 'fast';
+//   else return 'medium';
+// };
+
+// var getCommentHTML = function(content,currentIndex){
+//   return '<div class = "comment '+ getDuration() +'" style="top:'+ (currentIndex%15+1) * 5.5 +'%">'+content+'</div>'
+// }
+
+// var updateCurrentComments = function(currentTime){
+//   var num = 0;
+//   while (currentIndex < comments.length && comments[currentIndex].videoTime <= currentTime){
+//     if(currentTime === comments[currentIndex].videoTime && num<15){
+//       container.append(getCommentHTML(comments[currentIndex].comment, currentIndex));
+//       num ++;
+//     }
+//     currentIndex ++;
+//   };
+// };
